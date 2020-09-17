4a31f6c2b153eba3fb7f2642f6956823bc7b4f99
Opilane TTHK <opilane@tthk.ee>
2020-09-17 11:31:59 +0300

    Fixed code

---
+++ Speedometer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Speedometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program asks for distance and time as input and displays the speed in kilometers per hour*/
+            Console.WriteLine("Enter covered distance in km: ");
+            int distance = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter time spent in hour: ");
+            int time = Int32.Parse(Console.ReadLine());
+            int kph = distance / time;
+            Console.WriteLine($"You drove {kph}kph.");
+        }
+    }
+}
